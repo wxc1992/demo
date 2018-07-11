4fc97fa699ea1001ad0ab1440b67a2b4b5de58d2
wxc1992 <xupg126@126.com>
2018-07-11 20:32:34 +0800

    头部公用模块完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -1,10 +1,31 @@
 $(function(){
+    
     $(document).ajaxStart(function(){
         NProgress.start();
     })
 
     $(document).ajaxStop(function(){
         NProgress.done();
+    })
+
+    // 点击头部左边的icon菜单收回 
+    $('.left_toggle').click(function(){
+        $('body').toggleClass('active');
+        $('.left_menu').toggleClass('active');
+    })
+    $('.second').prev().on('click',function(){
+       $(this).next().slideToggle()
+    })
+    $('.logout').on('click',function(){
+        $('#myModal').modal('show');
+    })
+    $('.exit').on('click',function(){
+        $.get('/employee/employeeLogout',function(info){
+            console.log(info);
+            if(info){
+                location.href = 'login.html'
+            }
+        })
     })
 
 })
