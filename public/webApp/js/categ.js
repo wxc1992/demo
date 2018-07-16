b307f6bc8baaa0804b2158d412664ec68f7ddf04
wxc1992 <xupg126@126.com>
2018-07-16 13:10:45 +0800

    渲染分类

--- public/webApp/js/categ.js
+++ public/webApp/js/categ.js
@@ -1,3 +1,37 @@
-$('.left_box>li').on('click',function(){
-    console.log('haha')
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: '/category/queryTopCategory',
+        success: function (info) {
+            console.log(info);
+            $('.left_box  .mui-scroll').html(template('toplist', info));
+            console.log();
+            var id = info.rows[0].id;
+            second(id);
+            
+        }
+    })
+    
+    function second(id){
+        $.ajax({
+            type:'get',
+            url:'/category/querySecondCategory',
+            data:{
+                id:id
+            },
+            success:function(info){
+                console.log(info);
+                $('.right_box ul').html(template('content',info));
+            }
+        })
+    }
+
+    // 点击渲染第二级分类
+    $('.left_box').on('click','li',function(){
+        $(this).addClass('current').siblings().removeClass('current');
+        var id = $(this).data('id');
+        second(id);
+    })
+   
 })
+
